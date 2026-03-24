31a69345170a77a83ea51ecd5bfbd4e0f39844a0
Vitalya <vitalya.dev@gmail.com>
2026-03-24 15:20:21 +0300

    Значки добавлены на рабочий стол!

--- start.bat
+++ start.bat
@@ -15,6 +15,7 @@ echo  2 - Запустить Snappy Driver Installer (SDI)
 echo  3 - Установить Microsoft Office 2021
 echo  4 - Запустить Microsoft Activation Scripts
 echo  5 - Запустить Win11Debloat (Очистка Windows 11)
+echo  6 - Вывести иконки ПК, Панели управления и Office на рабочий стол
 echo.
 echo  0 - Выход
 echo.
@@ -29,6 +30,7 @@ if "%choice%"=="2" goto run_sdi
 if "%choice%"=="3" goto run_office
 if "%choice%"=="4" goto run_massgrave
 if "%choice%"=="5" goto run_debloat
+if "%choice%"=="6" goto run_icons
 if "%choice%"=="0" goto end
 
 :: Если ввели что-то другое
@@ -70,6 +72,31 @@ echo.
 echo Запускаю Win11Debloat в отдельном окне...
 :: Заменили двойные кавычки на одинарные вокруг ссылки, чтобы cmd не ругался
 start "" powershell -NoProfile -ExecutionPolicy Bypass -Command "& ([scriptblock]::Create((irm 'https://debloat.raphi.re/')))"
+goto main_menu
+
+
+:run_icons
+echo.
+echo Добавляю значки на рабочий стол...
+
+:: 1. Мой компьютер (Этот компьютер)
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f >nul
+
+:: 2. Панель управления
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{5399E694-6CE5-4D6C-8FCE-1D8870FDCBA0}" /t REG_DWORD /d 0 /f >nul
+
+:: 3. Ярлыки для Office (Word, Excel, PowerPoint) 
+:: (Предполагается стандартный путь установки Office 2016/2019/2021)
+powershell -NoProfile -Command "$s=(New-Object -COM WScript.Shell); $d=[Environment]::GetFolderPath('Desktop'); foreach($a in @('WINWORD','EXCEL','POWERPNT')){ $l=$s.CreateShortcut($d+'\'+$a+'.lnk'); $l.TargetPath='C:\Program Files\Microsoft Office\root\Office16\'+$a+'.exe'; $l.Save() }"
+
+:: 4. Перезапускаем Проводник, чтобы иконки появились моментально без перезагрузки ПК
+echo Перезапуск рабочего стола...
+taskkill /f /im explorer.exe >nul
+start explorer.exe
+
+echo.
+echo [УСПЕШНО] Значки добавлены на рабочий стол!
+pause
 goto main_menu
 
 :end
