3ffbeedf2e472bca7ca1d99bd1cdc204cb8bfb80
Vitalya <vitalya.dev@gmail.com>
2026-05-29 16:22:03 +0300

    refactor: собрать чистый автоматический режим с использованием вызовов call

--- start.bat
+++ start.bat
@@ -183,39 +183,20 @@ cls
 echo ===================================================
 echo           АВТОМАТИЧЕСКИЙ РЕЖИМ ЗАПУЩЕН
 echo ===================================================
-echo.
 
-echo [1/6] Запускаю Snappy Driver Installer...
-cd SDI_RUS\SDI
-start "" "SDI_x64_R2604.exe" -autoinstall
-cd ..\..
-
-echo [2/6] Запускаю MInstAll...
-cd core
-start "" cmd /c "install.bat"
-cd ..
-
-echo [3/6] Запускаю установку Microsoft Office 2021...
-cd "Microsoft Office LTSC 2021 Final + Project Pro + Visio Pro\Microsoft Office LTSC 2021 Final RUS x86_x64\ru_office_professional_plus_2021_x86_x64_dvd_2c455c8d"
-start "" "Setup.exe"
-cd /d "%~dp0"
+:: Вызываем модули (они сами выведут текст о своем запуске)
+call :run_sdi
+call :run_minstall
+call :run_office
 
 echo.
-echo [4/6] Ожидание установки драйверов, Wi-Fi и интернета...
+echo Ожидание установки драйверов, Wi-Fi и интернета...
 :wait_for_internet
-
-:: Постоянно пытаемся импортировать сети и подключиться.
-:: Если драйвер еще не встал, команды просто проигнорируются.
-if exist "wifi_profiles\Беспроводная сеть-VTI3.xml" (
-    netsh wlan add profile filename="wifi_profiles\Беспроводная сеть-VTI3.xml" >nul 2>&1
-)
-if exist "wifi_profiles\Беспроводная сеть-VTI3_Wi-Fi5.xml" (
-    netsh wlan add profile filename="wifi_profiles\Беспроводная сеть-VTI3_Wi-Fi5.xml" >nul 2>&1
-)
+if exist "wifi_profiles\Беспроводная сеть-VTI3.xml" netsh wlan add profile filename="wifi_profiles\Беспроводная сеть-VTI3.xml" >nul 2>&1
+if exist "wifi_profiles\Беспроводная сеть-VTI3_Wi-Fi5.xml" netsh wlan add profile filename="wifi_profiles\Беспроводная сеть-VTI3_Wi-Fi5.xml" >nul 2>&1
 netsh wlan connect name="VTI3" >nul 2>&1
 netsh wlan connect name="VTI3_Wi-Fi5" >nul 2>&1
 
-:: Проверяем, появился ли интернет после попытки подключения
 ping -n 1 -w 1000 8.8.8.8 >nul 2>&1
 if %errorlevel% neq 0 (
     timeout /t 5 >nul
@@ -223,12 +204,10 @@ if %errorlevel% neq 0 (
 )
 echo [+] Интернет подключен!
 
-echo.
-echo [5/6] Запускаю Win11Debloat...
-start "" powershell -NoProfile -ExecutionPolicy Bypass -Command "& ([scriptblock]::Create((irm 'https://debloat.raphi.re/'))) -CLI -Silent -RunDefaults"
+call :run_debloat
 
 echo.
-echo [6/6] Ожидание завершения установки Office 2021...
+echo Ожидание завершения установки Office 2021...
 :wait_for_office
 tasklist /fi "imagename eq setup.exe" | find /i "setup.exe" >nul
 if %errorlevel% equ 0 (
@@ -237,47 +216,17 @@ if %errorlevel% equ 0 (
 )
 echo [+] Установка Office завершена!
 
-echo.
-echo [+] Запускаю Microsoft Activation Scripts (оффлайн)...
-set "ZIP_FILE=%~dp0Microsoft-Activation-Scripts.zip"
-set "EXTRACT_TO=%~dp0Microsoft-Activation-Scripts"
-set "SEVEN_ZIP=%~dp07z.exe"
-set "ARCHIVE_PASSWORD=1"
+call :run_massgrave
 
-if not exist "%EXTRACT_TO%\MAS\All-In-One-Version-KL\MAS_AIO.cmd" (
-    if not exist "%ZIP_FILE%" (
-        echo [ОШИБКА] Архив "%ZIP_FILE%" не найден!
-        goto skip_mas
-    )
-    if not exist "%SEVEN_ZIP%" (
-        echo [ОШИБКА] 7z.exe не найден в корне флешки!
-        goto skip_mas
-    )
-    echo [-] Распаковываю запароленный архив MAS...
-    "%SEVEN_ZIP%" x "%ZIP_FILE%" -o"%EXTRACT_TO%" -p"%ARCHIVE_PASSWORD%" -y >nul
-)
-
-set "MAS_SCRIPT=%EXTRACT_TO%\MAS\All-In-One-Version-KL\MAS_AIO.cmd"
-if exist "%MAS_SCRIPT%" (
-    echo [+] Запускаю активацию Windows и Office...
-    start "" cmd /c "%MAS_SCRIPT%" /Z-WindowsESUOffice
-) else (
-    echo [ОШИБКА] Скрипт активации не найден!
-)
-:skip_mas
-
-echo.
-echo [+] Добавляю значки на рабочий стол...
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f >nul
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{5399E694-6CE5-4D6C-8FCE-1D8870FDCBA0}" /t REG_DWORD /d 0 /f >nul
-start "" powershell -NoProfile -Command "$d=[Environment]::GetFolderPath('Desktop'); $cp=[Environment]::GetFolderPath('CommonPrograms'); $up=[Environment]::GetFolderPath('Programs'); @('Word.lnk', 'Excel.lnk', 'PowerPoint.lnk') | ForEach-Object { $c=$cp+'\'+$_; $u=$up+'\'+$_; if(Test-Path $c){Copy-Item $c $d -Force} elseif(Test-Path $u){Copy-Item $u $d -Force} }"
+:: Трюк: передаем пустой ввод, чтобы пропустить команду pause внутри модуля run_icons
+echo. | call :run_icons
 
 echo.
 echo ===================================================
 echo     АВТОМАТИЧЕСКИЙ РЕЖИМ УСПЕШНО ЗАВЕРШЕН!
 echo ===================================================
 pause
-goto main_menu
+exit /b
 
 :end
 exit
